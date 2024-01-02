6bc1f1e96d7438500876e4ade3112718396f1138
marquezintop <marquez.cmbh@gmail.com>
2024-01-02 15:08:49 -0300

    feat: router component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const themeConfiguration = require('./src/theme/theme-configuration');
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: themeConfiguration.colors,
+    },
   },
   plugins: [],
 }
